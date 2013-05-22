ddf6ec5667ff8629570a78ad28e8539d00d2d7f1
Charles Flynn <cflynn@irab.org>
2013-05-21 22:00:17 -0500

    cleaned up dir structure

--- files/srv/salt/agiledata/env.sh
+++
@@ -1,2 +0,0 @@
-export PATH=$HOME/agiledata/software/java/bin:$HOME/agiledata/software/maven/bin:$HOME/agiledata/software/pig/bin:$HOME/agiledata/software/mongodb/bin:$HOME/agiledata/software/elasticsearch/bin:$PATH
-export JAVA_HOME=$HOME/agiledata/software/java
